443ea59511ab8df4e5624977c27b08ed044655c1
Rady119 <z2009@yahoo.com>
2025-07-26 05:28:07 +0300

    Add .next/static/chunks/src_app_tool_[toolName]_page_tsx_a993527f._.js 1753496883766

---
+++ .next/static/chunks/src_app_tool_[toolName]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e4f4645b._.js",
+  "static/chunks/src_5a2a5bd0._.js"
+],
+    source: "dynamic"
+});
